5f510d0c1fb76b0076eec7666c5261df20dd68aa
HeroKenn <xsh666@buaa.edu.cn>
2023-05-09 10:00:47 +0800

    v1

--- scripts/simpleAccount/transfer.ts
+++ scripts/simpleAccount/transfer.ts
@@ -5,33 +5,33 @@ import { CLIOpts } from "../../src";
 import config from "../../config.json";
 
 export default async function main(t: string, amt: string, opts: CLIOpts) {
-  const paymaster = opts.withPM
-    ? Presets.Middleware.verifyingPaymaster(
-        config.paymaster.rpcUrl,
-        config.paymaster.context
-      )
-    : undefined;
-  const simpleAccount = await Presets.Builder.SimpleAccount.init(
-    config.signingKey,
-    config.rpcUrl,
-    config.entryPoint,
-    config.simpleAccountFactory,
-    paymaster
-  );
-  const client = await Client.init(config.rpcUrl, config.entryPoint);
+    const paymaster = opts.withPM
+        ? Presets.Middleware.verifyingPaymaster(
+            config.paymaster.rpcUrl,
+            config.paymaster.context
+        )
+        : undefined;
+    const simpleAccount = await Presets.Builder.SimpleAccount.init(
+        config.signingKey,
+        config.rpcUrl,
+        config.entryPoint,
+        config.simpleAccountFactory,
+        paymaster
+    );
+    const client = await Client.init(config.rpcUrl, config.entryPoint);
 
-  const target = ethers.utils.getAddress(t);
-  const value = ethers.utils.parseEther(amt);
-  const res = await client.sendUserOperation(
-    simpleAccount.execute(target, value, "0x"),
-    {
-      dryRun: opts.dryRun,
-      onBuild: (op) => console.log("Signed UserOperation:", op),
-    }
-  );
-  console.log(`UserOpHash: ${res.userOpHash}`);
+    const target = ethers.utils.getAddress(t);
+    const value = ethers.utils.parseEther(amt);
+    const res = await client.sendUserOperation(
+        simpleAccount.execute(target, value, "0x"),
+        {
+            dryRun: opts.dryRun,
+            onBuild: (op) => console.log("Signed UserOperation:", op),
+        }
+    );
+    console.log(`UserOpHash: ${res.userOpHash}`);
 
-  console.log("Waiting for transaction...");
-  const ev = await res.wait();
-  console.log(`Transaction hash: ${ev?.transactionHash ?? null}`);
+    console.log("Waiting for transaction...");
+    const ev = await res.wait();
+    console.log(`Transaction hash: ${ev?.transactionHash ?? null}`);
 }
